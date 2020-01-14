e32222fac354cab097975374bddf664153b3771b
Povrty <iakwan@s.sfusd.edu>
2020-01-14 02:21:50 +0000

    Repeat steps for other inputs.  Everything else is finished.

--- README.md
+++ README.md
@@ -12,13 +12,13 @@ In this unit, coders will create the classic game of tic tac toe in their browse
 #### Set-Up
 - [x] Go to the repository
 - [x] Fork this repository to your github account and clone to a new workspace
-- [ ] Add, commit, and push your changes
+- [x] Add, commit, and push your changes
 - [x] Make your site live on gh-pages
 - [x] Submit your website using the link on the Agenda
 
 ### HTML/JavaScript
 - [x] Create a div to display your songs
-- [ ] Create an array to store your song names
+- [x] Create an array to store your song names
 - [ ] Create a loop to display your song names
 - [ ] Repeat these steps for song artist, song length, image, and link
 
--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
-let songNames = ["Stan"];
+let songList = [];
+let artistList = [];
+let lengthList = [];
+let imageURLs = [];
+let linkList = [];
 
 $(".addSong").click(function(){
-
-})
-
-songNames.forEach(function(songs){
-    $(".songList").append(songs);
-})
+    // Song Name:
+    var songName = $(".songName").val();
+        songList.push(songName);
+        $(".songLists").empty();
+        songList.forEach(function(songs){
+            $(".songLists").append("<li>" + songs + "</li>");
+        })
+    // Artist Name:
+    var artistName = $(".artistName").val();
+        artistList.push(artistName);
+        $(".artistLists").empty();
+        artistList.forEach(function(artists){
+            $(".artistLists").append("<li>" + artists + "</li>");
+        })
+});
